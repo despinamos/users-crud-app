d8ebd8da501646a0fad6f5b59fe07550045ac933
D. Moschokarfi <despinamx16@gmail.com>
2025-04-08 10:51:59 +0300

    feat: swagger.js completed winston logger added

--- usersApp/controllers/user.controller.js
+++ usersApp/controllers/user.controller.js
@@ -2,6 +2,8 @@ const User = require('../models/user.model');
 const userService = require('../services/user.services');
 const bcrypt = require('bcrypt');
 
+const logger = require('../logger/logger');
+
 exports.findAll = async(req, res) => {
   console.log("Find all users from collection users");
 
@@ -9,8 +11,10 @@ exports.findAll = async(req, res) => {
     // const result = await User.find();
     const result = await userService.findAll();
     res.status(200).json({status: true, data: result});
+    logger.info("INFO, Success in reading all users");
   } catch (err) {
     console.log("Problem in reading users", err);
+    logger.error("ERROR, Failed to read all users", err)
     res.status(400).json({status:false, data: err});
   }
 }
