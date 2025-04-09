8c61b1c00d7bd91d3bda681a9e933ad773d6b14a
D. Moschokarfi <despinamx16@gmail.com>
2025-04-09 20:29:11 +0300

    feat: google login

--- usersApp/routes/auth.routes.js
+++ usersApp/routes/auth.routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const authController = require('../controllers/auth.controller');
 
-router.post('/login', authController.login)
+router.post('/login', authController.login);
+router.get('/google/callback', authController.googleLogin);
 
 module.exports = router
+
+// https://accounts.google.com/o/oauth2/auth?client_id=748769172137-3mo8a59b35tib4gob14nbo165g1mfnoh.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
